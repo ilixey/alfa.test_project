82a377eba15e2a7cb9964064056afa6547bb0f3c
Ilya Vospyakov <vospyakov1976@mail.ru>
2022-06-13 17:56:45 +0300

    create GifService

--- src/main/java/com/alfa/test_project/controller/CurrencyController.java
+++ src/main/java/com/alfa/test_project/controller/CurrencyController.java
@@ -4,16 +4,21 @@ package com.alfa.test_project.controller;
 import com.alfa.test_project.dto.CurrencyDto;
 import com.alfa.test_project.service.CurrencyService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/currency-rates")
+@RequestMapping("/currencies-rates")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
 
-    @GetMapping
+    @GetMapping()
     public CurrencyDto getLatestRateByCurrencyCode(@RequestParam String code){
         return currencyService.getLatestRateByCurrencyCode(code);
     }
